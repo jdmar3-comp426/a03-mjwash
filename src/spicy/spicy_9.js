35354538b98e3e0396c8e05bb20b0b41e603a079
mjwash <mjwash@live.unc.edu>
2021-11-07 14:46:11 -0500

    spicy 1.3

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -55,7 +55,9 @@ export const multiplyBy = (num1) => {
  * Use the multiplyBy function to create and export a function named
  *   "tenTimes" that multiplies a number by 10.
  */
-export const tenTimes = multiplyBy()(10);
+export const tenTimes = (num1) => {
+    return multiplyBy(num1)(10);
+};
 
 
 /**
@@ -63,7 +65,7 @@ export const tenTimes = multiplyBy()(10);
  *   function to multiply 50 by 10 and returns the result.
  */
 export const tenTimesFifty = () => {
-
+    return tenTimes(50);
 };
 
 
