18af11a0f920d5e946cb815a5ce2dcaadfadd817
mjwash <mjwash@live.unc.edu>
2021-11-07 14:58:15 -0500

    spicy 1.4

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -153,7 +153,16 @@ export const someEven = (arr, test) => {
  *       -->  { pass: [1, 5, 31], fail: [90] }
  */
 export const filter = (arr, test) => {
-
+    let p = [], f = [];
+    for (let element of arr) {
+        if (test(element)) {
+            p.push(element);
+        } else {
+            f.push(element);
+        }
+    }
+    const filtered = {pass: p, fail: f};
+    return filtered;
 };
 
 
@@ -163,7 +172,7 @@ export const filter = (arr, test) => {
  *   odd numbers. Use the "everyEven" function in this function.
  */
 export const allEvensAreOdd = (arr) => {
-
+    return everyEven(arr, x => (x % 2) === 1);
 };
 
 
@@ -173,7 +182,7 @@ export const allEvensAreOdd = (arr) => {
  *   array is an odd number. Use the "someEven" function in this function.
  */
 export const anEvenIsOdd = (arr) => {
-
+    return someEven(arr, x => (x % 2) === 1);
 };
 
 
